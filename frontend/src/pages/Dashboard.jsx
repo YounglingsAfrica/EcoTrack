8b5ef07732d3c6269956b94b5df839ff82fdb716
Dean Heeger <127305010+DeanHeeger@users.noreply.github.com>
2023-10-30 14:08:43 +0200

    Update Dashboard.jsx
    Dashboard Head complete

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { FaBell } from "react-icons/fa";
+import { FaCircleUser } from "react-icons/fa6";
 
 const Dashboard = () => {
     return(
-        <div className='p-7'>
-            <h1 className='text-white text-2xl font-semibold'>Admin Dashboard</h1>
-        </div>
+        <>
+            <div className="flex items-center justify-between h-[70px] shadow-lg px-[25px]">
+                <div className='p-7'>
+                    <h1 className='text-white text-2xl font-semibold'>Admin Dashboard</h1>
+                </div>
+                <div className="flex items-center gap-[15px] relative">
+                    <div className="border-r-[1px] pr-[25px]">
+                        <p className="text-white">Dwayne</p>
+                    </div>
+                    <div className="flex items-center gap-[25px] flex items-center justify-center relative">
+                        <FaCircleUser className="text-white text-3xl cursor-pointer" />
+                        <div>
+                            <FaBell className="text-white text-2xl cursor-pointer" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+
     );
 };
 
