5b98d1b3a4a00dc101c6cb17d70607f4aa71fd0e
Keenan Deyce <keenandeyce@gmail.com>
2023-11-05 19:48:57 +0200

    Update userContext.jsx

--- frontend/src/context/userContext.jsx
+++ frontend/src/context/userContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useState, useEffect } from "react";
 export const UserContext = createContext({})
 
 export function UserContextProvider({children}) {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState();
 
     // Disable react-hooks/exhaustive-deps for this useEffect
     /* eslint-disable react-hooks/exhaustive-deps */
