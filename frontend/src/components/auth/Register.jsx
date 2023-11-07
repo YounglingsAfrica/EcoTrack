78d59b9ad5290069a4c735bed70a9536f96394b5
Keenan Deyce <keenandeyce@gmail.com>
2023-11-07 21:09:57 +0200

    Update Register.jsx

--- frontend/src/components/auth/Register.jsx
+++ frontend/src/components/auth/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import Logo from "../../assets/eco_logo.png";
 import Eco from "../../assets/ecoeco.png";
@@ -8,7 +8,7 @@ import { toast } from "react-hot-toast";
 
 const SignUp = () => {
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const [data, setData] = useState({
         name: '',
         email: '',
