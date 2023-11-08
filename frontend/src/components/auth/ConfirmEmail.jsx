99fb72487aacd2e6c52e1ff8c8692e439de4c9f4
Keenan Deyce <keenandeyce@gmail.com>
2023-11-08 08:48:09 +0200

    Updates

--- frontend/src/components/auth/ConfirmEmail.jsx
+++ frontend/src/components/auth/ConfirmEmail.jsx
@@ -7,18 +7,20 @@ import axios from "axios";
 
 const ConfirmEmail = () => {
     const [status, setStatus] = useState("Verifying...");
-    const { token } = useParams();
+    const { id, token } = useParams();
 
     useEffect(() => {
-        axios.get(`/confirm/${token}`, { withCredentials: true })
+        axios.get(`/confirm/${id}/${token}`, { withCredentials: true })
             .then(res => {
                 console.log('Response from backend:', res.data);
                 setStatus(res.data.message);
             })
             .catch(err => {
-                setStatus("An error ocurred");
+                setStatus("No Status");
             })
-    }, [token]);
+    }, [id, token]);
+
+    console.log(status);
 
     return (
         <div className="min-h-screen pt-40 bg-black">
@@ -30,10 +32,17 @@ const ConfirmEmail = () => {
                             <img src={X} alt="exit" className='h-auto w-4 absolute top-4 left-4 hover:scale-75 duration-300' />
                         </Link> 
                         <img src={Logo} alt="Eco" className='mx-auto w-32' />
-                        <h2 className="text-3xl mb-6 mt-6 text-center">Confirming your email address..</h2>
-                        <p className="mb-4 text-center text-sm">
-                            {status}
-                        </p>
+                        <h2 className="text-3xl mb-6 mt-6 text-center">Your email has been verified, please log in.</h2>
+                        {/* insert button to login */}
+                        <div className="mt-6 flex justify-center">
+                            <Link to="/login">
+                                <button 
+                                    className="flex items-center justify-center px-6 w-auto h-10 text-white rounded-lg bg-gradient-to-r from-black to-primaryGreen shadow-right-bottom"
+                                >
+                                    Login
+                                </button>
+                            </Link>
+                        </div>
                     </div>
                     <div className='w-1/2'>
                         <img className='w-auto h-full object-cover' src={Eco} alt="Eco" />
