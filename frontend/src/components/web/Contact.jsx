130953a8c828db0c89019f9453277b62d2e35568
Keenan Deyce <keenandeyce@gmail.com>
2023-11-08 17:25:25 +0200

    Update Contact.jsx

--- frontend/src/components/web/Contact.jsx
+++ frontend/src/components/web/Contact.jsx
@@ -99,7 +99,8 @@ const Contact = () => {
                                 required
                             />
                         </form>
-                            <button 
+                            <button
+                                onClick={contactFormSubmit} 
                                 className='flex items-center text-center justify-center px-6 w-56 h-14 text-white rounded-xl bg-gradient-to-r from-black to-primaryGreen mt-10 mx-auto shadow-right-bottom'
                             >
                                 <svg className='h-auto w-6 mr-4' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M1.94631 9.31555C1.42377 9.14137 1.41965 8.86034 1.95706 8.6812L21.0433 2.31913C21.5717 2.14297 21.8748 2.43878 21.7268 2.95706L16.2736 22.0433C16.1226 22.5718 15.8179 22.5901 15.5946 22.0877L12.0002 14.0002L18.0002 6.00017L10.0002 12.0002L1.94631 9.31555Z" fill="rgba(255,255,255,1)"></path></svg>Send Message
