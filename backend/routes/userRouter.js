cc74e7c9ee238cf444efe60158225e01aa0dae86
Keenan Deyce <keenandeyce@gmail.com>
2023-11-13 11:28:59 +0200

    Update userRouter.js

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -16,6 +16,10 @@ const path = require("path");
 const multer = require("multer");
 const uploadsDir = path.join(__dirname, 'uploads');
 
+if (!fs.existsSync(uploadsDir)){
+  fs.mkdirSync(uploadsDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, uploadsDir)
