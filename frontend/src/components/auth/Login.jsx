c0faf1440a626d04e29529c85759e81b9b155ca3
Keenan Deyce <keenandeyce@gmail.com>
2023-11-15 10:20:40 +0200

    Update Login.jsx

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
         const { email, password } = data;
     
         try {
-            const response = await axios.post("https://breakable-hosiery-ant.cyclic.app/login", { 
+            const response = await axios.post("/login", { 
                 email, 
                 password 
             });
