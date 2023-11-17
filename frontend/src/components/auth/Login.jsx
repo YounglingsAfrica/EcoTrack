d1497a47436b9821f996a306543b06f28358e465
Keenan Deyce <keenandeyce@gmail.com>
2023-11-17 07:57:36 +0200

    Update Login.jsx

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -132,7 +132,7 @@ const Login = () => {
                                     OR
                                 </p>
                             </div>
-                            <div className="px-6 sm:px-0 max-w-sm mx-auto mt-6">
+                            {/*<div className="px-6 sm:px-0 max-w-sm mx-auto mt-6">
                                 <button 
                                     type="button" 
                                     className="text-white w-full  bg-[#000] hover:scale-105 duration-300 focus:ring-4 focus:outline-none focus:ring-primaryGreen/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-between dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
@@ -141,7 +141,7 @@ const Login = () => {
                                         Continue with Google
                                     <div></div>
                                 </button>
-                            </div>
+                                </div>*/}
                         </form>
                     </div>
                     <div className='w-1/2'>
