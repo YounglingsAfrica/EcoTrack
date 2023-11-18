7bcb01f1a661094191ae45d69b914e74b0447235
Keenan Deyce <keenandeyce@gmail.com>
2023-11-18 16:43:24 +0200

    Update userContext.jsx

--- frontend/src/context/userContext.jsx
+++ frontend/src/context/userContext.jsx
@@ -50,9 +50,3 @@ export function UserContextProvider({children}) {
         </UserContext.Provider>
     )
 }
-
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-}
