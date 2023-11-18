1ec2bf8f755bba80d322e071291bf73b0d0e7702
Keenan Deyce <keenandeyce@gmail.com>
2023-11-18 17:11:18 +0200

    Update userContext.jsx

--- frontend/src/context/userContext.jsx
+++ frontend/src/context/userContext.jsx
@@ -1,11 +1,9 @@
 import axios from "axios";
 import { createContext, useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 export const UserContext = createContext({})
 
 export function UserContextProvider({children}) {
-    const navigate = useNavigate();
     const [user, setUser] = useState(null);
     const fetchUserProfile = () => {
         axios.get('/profile', { withCredentials: true })
@@ -14,29 +12,8 @@ export function UserContextProvider({children}) {
             setUser(data);
         })
         .catch(error => {
-            if (error.response && error.response.status === 403) {
-                // Token is expired, refresh it
-                axios.post('/token', {}, { withCredentials: true })
-                .then(({ data }) => {
-                    // Save the new token
-                    const authToken = data.authToken;
-                    navigate("/dashboard-b")
-                    // Retry the /profile request with the new token
-                    axios.get('/profile', { headers: { 'Authorization': `Bearer ${authToken}` }, withCredentials: true })
-                    .then(({ data }) => {
-                        data.avatar = `/avatar/${data.id}`;
-                        setUser(data);
-                    })
-                })
-                .catch(error => {
-                    console.error(error);
-                    navigate("/login")
-                    // If refresh token is also expired, redirect to login page or handle appropriately
-                });
-            } else {
-                console.error(error); 
-            }
-        });
+            console.error(error); 
+        })
     };
     
     useEffect(() => {
