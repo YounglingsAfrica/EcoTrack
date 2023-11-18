b4dfb694461f24e927cee7e3a09da91568cc6558
Keenan Deyce <keenandeyce@gmail.com>
2023-11-18 14:28:07 +0200

    Update userContext.jsx

--- frontend/src/context/userContext.jsx
+++ frontend/src/context/userContext.jsx
@@ -5,26 +5,44 @@ export const UserContext = createContext({})
 
 export function UserContextProvider({children}) {
     const [user, setUser] = useState(null);
+    const fetchUserProfile = () => {
+        axios.get('/profile', { withCredentials: true })
+        .catch(error => {
+            if (error.response.status === 403) {
+                // Token is expired, refresh it
+                const refreshToken = getCookie('refreshToken');
+                axios.post('/token', { token: refreshToken }, { withCredentials: true })
+                .then(({ data }) => {
+                    // Save the new token
+                    const authToken = data.authToken;
+        
+                    // Retry the /profile request with the new token
+                    axios.get('/profile', { headers: { 'Authorization': `Bearer ${authToken}` } })
+                    .then(({ data }) => {
+                        data.avatar = `/avatar/${data.id}`;
+                        setUser(data);
+                    })
+                })
+            } else {
+                console.error(error); 
+            }
+        });
+    };
     
     useEffect(() => {
-        if (user) {
-            axios.get("/profile", { withCredentials: true }).then(({data}) => {
-                data.avatar = `/avatar/${data.id}`;
-                setUser(data);
-            }).catch(error => {
-                if (error.response.status === 401) {    
-                    console.error("User is not authenticated");
-                } else {
-                    console.error(error);
-                }
-            })
-        }   
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+        fetchUserProfile();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])  
 
     return (
-        <UserContext.Provider value={{user, setUser}}>
+        <UserContext.Provider value={{user, setUser, fetchUserProfile}}>
             {children}
         </UserContext.Provider>
     )
+}
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
 }
