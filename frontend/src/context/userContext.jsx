45678e5a28b54db9175ca24c562543815fe71099
Keenan Deyce <keenandeyce@gmail.com>
2023-11-19 17:16:35 +0200

    Update userContext.jsx

--- frontend/src/context/userContext.jsx
+++ frontend/src/context/userContext.jsx
@@ -8,8 +8,15 @@ export function UserContextProvider({children}) {
     const fetchUserProfile = () => {
         axios.get('/profile', { withCredentials: true })
         .then(({ data }) => {
-            data.avatar = `/avatar/${data.id}`;
-            setUser(data);
+            // Fetch the signed URL
+            axios.get(`/avatar/${data.id}`, { withCredentials: true })
+                .then(({ request: { responseURL } }) => {
+                    data.avatar = responseURL;
+                    setUser(data);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
         })
         .catch(error => {
             console.error(error); 
