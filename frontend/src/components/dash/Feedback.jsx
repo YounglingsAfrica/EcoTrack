245a7827e25957b062be9de08a5d30d9c5dbb7a5
Keenan Deyce <keenandeyce@gmail.com>
2024-01-16 10:02:32 +0200

    Update Feedback.jsx

--- frontend/src/components/dash/Feedback.jsx
+++ frontend/src/components/dash/Feedback.jsx
@@ -29,6 +29,13 @@ const Feedback = () => {
       .then((response) => {
         if (response.status === 200) {
           console.log('Feedback Submitted');
+          setFormData({
+            frequency: "",
+            mostUsedFeature: "",
+            improvementSuggestion: "",
+            motivation: "",
+            followUp: false,
+          })
           toast.success('Feedback Submitted')
         } else {
           console.error('Error submitting feedback')
@@ -44,60 +51,62 @@ const Feedback = () => {
       <div className='grid-cols-2 h-[85vh] gap-[25px] flex'>
         <div className='basis-[65%] flex-wrap bg-white rounded-lg p-6 px-14'>
             <h2 className='pb-8 text-4xl font-bold leading-snug flex items-center justify-center'>Help us improve</h2>
-            <input 
-              className='border-b-2 border-gray-400 w-full focus:outline-none focus:border-primaryGreen mb-20' 
-              type="text" 
-              name="frequency"
-              value={formData.frequency}
-              onChange={handleChange}
-              placeholder='How often do you use our website?'
-            />
-            <input 
-              className='border-b-2 border-gray-400 w-full focus:outline-none focus:border-primaryGreen mb-20' 
-              type="text"
-              name='mostUsedFeature'
-              value={formData.mostUsedFeature}
-              onChange={handleChange}
-              placeholder='What is your most used feature?'
-            />
-            <input 
-              className='border-b-2 border-gray-400 w-full focus:outline-none focus:border-primaryGreen mb-20' 
-              type="text" 
-              name='improvementSuggestion'
-              value={formData.improvementSuggestion}
-              onChange={handleChange}
-              placeholder='What would you like to see improved the most?'
-            />
-            <textarea
-              className='border-b-2 border-gray-400 w-full focus:outline-none focus:border-primaryGreen mb-8' 
-              type="text"
-              name='motivation'
-              value={formData.motivation}
-              onChange={handleChange}
-              placeholder='What is your motivation to use our website?'
-            />
-            <label className="flex items-center mb-4">
+            <form onSubmit={handleSubmit}>
               <input 
-                type="checkbox" 
-                className="form-checkbox text-primaryGreen cursor-pointer" 
-                name="followUp"
-                checked={formData.followUp}
-                onChange={handleChange} 
+                className='border-b-2 border-gray-400 w-full focus:outline-none focus:border-primaryGreen mb-20' 
+                type="text" 
+                name="frequency"
+                value={formData.frequency}
+                onChange={handleChange}
+                placeholder='How often do you use our website?'
               />
-                <span 
-                  className="ml-2 text-sm font-medium text-darkGreen cursor-pointer underline"
-                >
-                  Receive a personal follow-up to your feedback?
-                </span>
-              </label>
-            <button 
-              className='flex items-center text-center justify-center px-6 w-56 h-14 mt-6 text-white rounded-xl bg-gradient-to-r from-black to-primaryGreen mx-auto shadow-right-bottom hover:shadow-sm transform hover:scale-[103%] transition duration-300 ease-out'
-              onSubmit={handleSubmit}
-            >
-              <svg className='h-auto w-6 mr-4' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M1.94631 9.31555C1.42377 9.14137 1.41965 8.86034 1.95706 8.6812L21.0433 2.31913C21.5717 2.14297 21.8748 2.43878 21.7268 2.95706L16.2736 22.0433C16.1226 22.5718 15.8179 22.5901 15.5946 22.0877L12.0002 14.0002L18.0002 6.00017L10.0002 12.0002L1.94631 9.31555Z" fill="rgba(255,255,255,1)"></path>
-              </svg>
-                Submit Feedback
-            </button>
+              <input 
+                className='border-b-2 border-gray-400 w-full focus:outline-none focus:border-primaryGreen mb-20' 
+                type="text"
+                name='mostUsedFeature'
+                value={formData.mostUsedFeature}
+                onChange={handleChange}
+                placeholder='What is your most used feature?'
+              />
+              <input 
+                className='border-b-2 border-gray-400 w-full focus:outline-none focus:border-primaryGreen mb-20' 
+                type="text" 
+                name='improvementSuggestion'
+                value={formData.improvementSuggestion}
+                onChange={handleChange}
+                placeholder='What would you like to see improved the most?'
+              />
+              <textarea
+                className='border-b-2 border-gray-400 w-full focus:outline-none focus:border-primaryGreen mb-8' 
+                type="text"
+                name='motivation'
+                value={formData.motivation}
+                onChange={handleChange}
+                placeholder='What is your motivation to use our website?'
+              />
+              <label className="flex items-center mb-4">
+                <input 
+                  type="checkbox" 
+                  className="form-checkbox text-primaryGreen cursor-pointer" 
+                  name="followUp"
+                  checked={formData.followUp}
+                  onChange={handleChange} 
+                />
+                  <span 
+                    className="ml-2 text-sm font-medium text-darkGreen cursor-pointer underline"
+                  >
+                    Receive a personal follow-up to your feedback?
+                  </span>
+                </label>
+              <button 
+                className='flex items-center text-center justify-center px-6 w-56 h-14 mt-6 text-white rounded-xl bg-gradient-to-r from-black to-primaryGreen mx-auto shadow-right-bottom hover:shadow-sm transform hover:scale-[103%] transition duration-300 ease-out'
+                type='submit'
+              >
+                <svg className='h-auto w-6 mr-4' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M1.94631 9.31555C1.42377 9.14137 1.41965 8.86034 1.95706 8.6812L21.0433 2.31913C21.5717 2.14297 21.8748 2.43878 21.7268 2.95706L16.2736 22.0433C16.1226 22.5718 15.8179 22.5901 15.5946 22.0877L12.0002 14.0002L18.0002 6.00017L10.0002 12.0002L1.94631 9.31555Z" fill="rgba(255,255,255,1)"></path>
+                </svg>
+                  Submit Feedback
+              </button>
+            </form>
         </div>
         <div className='basis-[45%] h-[65vh] flex items-center justify-center m-8'>
           <img 
