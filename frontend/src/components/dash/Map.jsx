0b0dabe9271e23672158973a5f8d15f8703ce586
Dean Heeger <127305010+DeanHeeger@users.noreply.github.com>
2024-01-30 13:58:57 +0200

    Update Map.jsx
    removed unwanted code.

--- frontend/src/components/dash/Map.jsx
+++ frontend/src/components/dash/Map.jsx
@@ -1,26 +1,3 @@
-/*import React from 'react';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
-import 'leaflet-defaulticon-compatibility';
-import 'leaflet/dist/leaflet.css';
-
-const Map = () => {
-  return (
-    <div className="h-[86vh] sticky">
-      <MapContainer center={[-33.919667, 18.419253]} zoom={13} className="h-full">
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
-        />
-        <Marker position={[-33.919667, 18.419253]}></Marker>
-      </MapContainer>
-    </div>
-  );
-};
-
-export default Map;*/
-
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
